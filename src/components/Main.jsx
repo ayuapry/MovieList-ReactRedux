4e1edff6c4942fbf99524c630d4871bde299454b
Ayuapry <ayuapriyanti0304@gmail.com>
2022-10-04 19:56:23 +0700

    add header

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
+import {AiOutlinePlayCircle} from 'react-icons/ai'
 
 export const Main = () => {
 const [movies, setMovies] = useState([])
@@ -12,12 +13,31 @@ useEffect (() => {
         setMovies(response.data.results)
     })
 }, [])
-console.log(movie)
+// console.log(movie)
+
+const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + '...';
+    } else {
+      return str;
+    }
+  };
 
   return (
-    <div className='w-full h-[550px] text-white '>
+    <div className='w-full h-[600px] text-white '>
         <div className='w-full h-full'>
+            <div className='absolute w-full h-[600px] bg-gradient-to-r from-black '></div>
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            <div className='absolute w-full top-[20%] p-4 md:p-8 '>
+                <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+            <p className='text-gray-400 text-sm my-4'>Released: {movie?.release_date}</p>
+            <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'> {truncateString(movie?.overview,200)}</p>
+            <div className='my-4'>
+                <button className=' bg-red-600 rounded-full text-white hover:bg-red-400 hidden md:flex pl-5 py-3 mr-4 w-[170px]'>
+                <AiOutlinePlayCircle size={25} className='mr-2' />Watch Trailer 
+                </button>
+            </div>
+            </div>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>MovieList</h1>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer ml-3'>MovieList</h1>
         <div>
-            <button className='text-white px-6 py-4 bg-transparent rounded-full border-2 border-red-600'>Login</button>
-            <button className='bg-red-600 px-6 py-4 rounded-full text-white'>Register</button>
+            <button className='text-white px-8 py-2 mr-2 bg-transparent rounded-full border-2 border-red-500'>Login</button>
+            <button className='bg-red-600 px-8 py-2 rounded-full text-white'>Register</button>
         </div>
     </div>
   )
