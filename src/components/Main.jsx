54721c974c527feb10a19d9a3d5fd391e9bb5257
Ayuapry <ayuapriyanti0304@gmail.com>
2022-10-05 14:24:00 +0700

    add swipper

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,19 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
 import {AiOutlinePlayCircle} from 'react-icons/ai'
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 export const Main = () => {
 const [movies, setMovies] = useState([]);
 
+//hero
 const movie = movies[Math.floor(Math.random() * movies.length)]
+const movie1 = movies[Math.floor(Math.random() * movies.length)]
+const movie2 = movies[Math.floor(Math.random() * movies.length)]
 
 useEffect (() => {
     axios.get(requests.requestPopular).then((response) => {
@@ -27,17 +35,47 @@ const truncateString = (str, num) => {
     <div className='w-full h-[600px] text-white '>
         <div className='w-full h-full'>
             <div className='absolute w-full h-[600px] bg-gradient-to-r from-black '></div>
-            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-            <div className='absolute w-full top-[20%] p-4 md:p-8 '>
-                <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
-            <p className='text-gray-400 text-sm my-4'>Released: {movie?.release_date}</p>
-            <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'> {truncateString(movie?.overview,200)}</p>
-            <div className='my-4'>
-                <button className=' bg-red-600 rounded-full text-white hover:bg-red-400 hidden md:flex pl-5 py-3 mr-4 w-[170px]'>
-                <AiOutlinePlayCircle size={25} className='mr-2' />Watch Trailer 
-                </button>
-            </div>
-            </div>
+            <Swiper pagination={{ dynamicBullets: true, }} modules={[Pagination]} className="mySwiper w-full h-[600px]">
+                <SwiperSlide>
+                  <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+                  <div className='absolute w-full top-[20%] p-4 md:p-8 '>
+                    <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+                    <p className='text-gray-400 text-sm my-4'>Released: {movie?.release_date}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'> {truncateString(movie?.overview,200)}</p>
+                    <div className='my-4'>
+                        <button className=' bg-red-600 rounded-full text-white hover:bg-red-400 hidden md:flex pl-5 py-3 mr-4 w-[170px]'>
+                        <AiOutlinePlayCircle size={25} className='mr-2' />Watch Trailer 
+                        </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='w-full h-full object-cover ' src={`https://image.tmdb.org/t/p/original/${movie1?.backdrop_path}`} alt={movie1?.title} />
+                  <div className='absolute w-full top-[20%] p-4 md:p-8 '>
+                    <h1 className='text-3xl md:text-5xl font-bold'>{movie1?.title}</h1>
+                    <p className='text-gray-400 text-sm my-4'>Released: {movie1?.release_date}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'> {truncateString(movie1?.overview,200)}</p>
+                    <div className='my-4'>
+                        <button className=' bg-red-600 rounded-full text-white hover:bg-red-400 hidden md:flex pl-5 py-3 mr-4 w-[170px]'>
+                        <AiOutlinePlayCircle size={25} className='mr-2' />Watch Trailer 
+                        </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className='w-full h-full object-cover ' src={`https://image.tmdb.org/t/p/original/${movie2?.backdrop_path}`} alt={movie2?.title} />
+                  <div className='absolute w-full top-[20%] p-4 md:p-8 '>
+                    <h1 className='text-3xl md:text-5xl font-bold'>{movie2?.title}</h1>
+                    <p className='text-gray-400 text-sm my-4'>Released: {movie2?.release_date}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'> {truncateString(movie2?.overview,200)}</p>
+                    <div className='my-4'>
+                        <button className=' bg-red-600 rounded-full text-white hover:bg-red-400 hidden md:flex pl-5 py-3 mr-4 w-[170px]'>
+                        <AiOutlinePlayCircle size={25} className='mr-2' />Watch Trailer 
+                        </button>
+                    </div>
+                  </div>
+                </SwiperSlide>  
+            </Swiper>
         </div>
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ export const Search = () => {
   return (
     <div>
         <div className='bg-transparents border border-red-600 rounded-full flex items-center px-2 w-[200px] sm:w-[200px] lg:w-[300px]'>
-        <input className='bg-transparent p-2 w-full focus:outline-none' type="text"  placeholder='What do you want to watch?' />
+        <input className='bg-transparent p-2 w-full focus:outline-none text-white' type="text"  placeholder='What do you want to watch?' />
         <AiOutlineSearch  color='white' size={20} />
       </div>
     </div>
